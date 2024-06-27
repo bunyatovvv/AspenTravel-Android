479ff577d9446d7956b11605d6a4ba7c7bd7313f
Bunyatov <bunyatovbhmn@gmail.com>
2024-06-27 14:28:58 +0400

    support map fragment

---
+++ app/src/main/java/com/behmennnn/aspentravel/common/util/Utils.kt
@@ -0,0 +1,11 @@
+package com.behmennnn.aspentravel.common.util
+
+import com.google.android.gms.maps.SupportMapFragment
+
+class Utils {
+
+    companion object{
+        var mapFragment : SupportMapFragment?=null
+
+    }
+}
