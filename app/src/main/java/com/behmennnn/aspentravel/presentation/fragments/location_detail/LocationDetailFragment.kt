54e931fa24ac47c9fd57da56308bbf68b816fd60
Bunyatov <bunyatovbhmn@gmail.com>
2024-06-27 17:10:36 +0400

    feature adapter -> implemented

--- app/src/main/java/com/behmennnn/aspentravel/presentation/fragments/location_detail/LocationDetailFragment.kt
+++ app/src/main/java/com/behmennnn/aspentravel/presentation/fragments/location_detail/LocationDetailFragment.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract.RawContacts.Data
 import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.behmennnn.aspentravel.R
 import com.behmennnn.aspentravel.common.BaseFragment
 import com.behmennnn.aspentravel.common.BaseViewModel
@@ -16,12 +16,15 @@ import com.behmennnn.aspentravel.common.util.Status
 import com.behmennnn.aspentravel.common.util.gone
 import com.behmennnn.aspentravel.common.util.setImageURL
 import com.behmennnn.aspentravel.common.util.visible
+import com.behmennnn.aspentravel.data.dto.Feature
 import com.behmennnn.aspentravel.databinding.FragmentLocationDetailBinding
 import com.behmennnn.aspentravel.presentation.fragments.home.HomeViewModel
+import com.behmennnn.aspentravel.presentation.fragments.location_detail.adapter.FeatureAdapter
 
 class LocationDetailFragment : BaseFragment<FragmentLocationDetailBinding>(FragmentLocationDetailBinding::inflate) {
 
     private lateinit var baseViewModel: BaseViewModel
+    private val featureAdapter : FeatureAdapter by lazy { FeatureAdapter() }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -29,6 +32,11 @@ class LocationDetailFragment : BaseFragment<FragmentLocationDetailBinding>(Fragm
         baseViewModel = ViewModelProvider(requireActivity())[BaseViewModel::class.java]
 
         observeLiveData()
+        binding.featuresRv.adapter = featureAdapter
+        binding.featuresRv.layoutManager = LinearLayoutManager(requireActivity(),LinearLayoutManager.HORIZONTAL,false)
+
+        binding.featuresRv.gone()
+
         binding.backButton.setOnClickListener {
             findNavController().navigateUp()
         }
